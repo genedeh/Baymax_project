d19b1ac4bb3010e790eaf87f5a4a0e5b92d15a2b
KidChaos <genesis.enedeh@gmail.com>
2021-09-02 12:29:03 +0100

    put in a test for vocals

---
+++ test_area/test_vocals_and_commands.py
@@ -0,0 +1,11 @@
+from Vocal_and_commands.Vocals import Vocals
+import unittest
+
+
+class VocalsTest(unittest.TestCase):
+    def setUp(self):
+        self.vocal = Vocals()
+
+    def test_speak_type(self):
+        speech = self.vocal.speak('test')
+        self.assertIsInstance(speech, str)
