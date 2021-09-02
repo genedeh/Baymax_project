a4a3737c7bc000f3099adb544fbbbe141cd87681
KidChaos <genesis.enedeh@gmail.com>
2021-09-02 23:05:35 +0100

    put in a test class for abstraction test

--- infomation_and_commands/infomation_abstraction.py
+++ infomation_and_commands/infomation_abstraction.py
@@ -19,3 +19,25 @@ class InformationAbstraction(ABC):
     @abstractmethod
     def change_information(self, info_name, new_info_value):
         pass
+
+
+class TestInformation(InformationAbstraction):
+    def get_information(self, info_name):
+        if info_name in self.information:
+            return  self.information.get(info_name)
+        else:
+            return "not found"
+
+    def set_information(self, info_name, info_value):
+        if info_name not in self.information:
+            self.information[info_name] = info_value
+            return info_name
+        else:
+            return "found"
+
+    def change_information(self, info_name, new_info_value):
+        if info_name in self.information:
+            self.information[info_name] = new_info_value
+            return new_info_value
+        else:
+            return "not found"
