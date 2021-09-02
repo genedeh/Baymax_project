17e582a2bf8d4126fcd140fd9c125f10cc45c9d1
KidChaos <genesis.enedeh@gmail.com>
2021-09-02 11:50:17 +0100

    made a class called Vocals for handling speech and commands

---
+++ Vocal_and_commands/Vocals.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+import pyttsx3
+
+
+class Vocals:
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 160)
+        engine.setProperty('volume', 10.9)
+        print(text)
+        engine.say(text)
+        engine.runAndWait()
+
+    def take_command(self):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("||\/||\/||....")
+            audio = recognizer.listen(source)
+
+        try:
+            print("RECOGNIZING...")
+            query = recognizer.recognize_google(audio, language="en-uk")
+            print(f"{query.capitalize()}.\n")
+
+        except Exception as e:
+            self.speak("Sorry I did not here you, come again.")
+            query = None
+        return query
+
