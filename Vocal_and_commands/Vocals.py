760fad74a52ce512c5faa08920a6d187e4dbf2a8
KidChaos <genesis.enedeh@gmail.com>
2021-09-03 20:01:18 +0100

    refactor voice speech and commands

--- Vocal_and_commands/Vocals.py
+++ Vocal_and_commands/Vocals.py
@@ -5,8 +5,8 @@ import pyttsx3
 class Vocals:
     def speak(self, text):
         engine = pyttsx3.init()
-        engine.setProperty('rate', 160)
-        engine.setProperty('volume', 10.9)
+        engine.setProperty('rate', 100.10)
+        engine.setProperty('volume', 1.9)
         print(text)
         engine.say(text)
         engine.runAndWait()
@@ -15,15 +15,20 @@ class Vocals:
     def take_command(self):
         recognizer = sr.Recognizer()
         with sr.Microphone() as source:
-            print("||\/||\/||....")
-            audio = recognizer.listen(source)
+            print(f'==========\n"INITIALIZING"\n==========')
+            self.speak(r",I AM LISTENING....")
+            audio = recognizer.record(source, duration=3.10)
 
         try:
-            print("RECOGNIZING...")
-            query = recognizer.recognize_google(audio, language="en-uk")
+            self.speak("RECOGNIZING SPEECH...")
+            query = recognizer.recognize_google(audio, language="en-us")
             print(f"{query.capitalize()}.\n")
 
         except Exception as e:
             self.speak("Sorry I did not here you, come again.")
             query = None
         return query
+
+
+v = Vocals()
+v.take_command()
