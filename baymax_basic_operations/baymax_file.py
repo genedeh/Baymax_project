d4b93eb64ee8af5419895a63ffe6e33033bc85e3
KidChaos <genesis.enedeh@gmail.com>
2021-10-20 18:13:24 +0100

    put in a new method called read_file

--- baymax_basic_operations/baymax_file.py
+++ baymax_basic_operations/baymax_file.py
@@ -8,6 +8,13 @@ class FileManager:
         vocal = Vocals()
         vocal.speak(f'{filename}, has been created')
 
+    def read_file(self, filename: str):
+        with open(f'{filename}', 'r') as file:
+            content = file.read()
+        vocal = Vocals()
+        vocal.speak(f'{filename}, has been read')
+        return content
+
 
 fm = FileManager()
-fm.create_file('test.txt')
+print(fm.read_file('test.txt'))
