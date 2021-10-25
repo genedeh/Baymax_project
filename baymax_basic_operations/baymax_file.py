38eb5d964657d2cb33615cbb625b8b6ab067afd2
KidChaos <genesis.enedeh@gmail.com>
2021-10-25 20:19:09 +0100

    put in adjustment to FileManger and put in a method called create_dir for DirectoryManger class

--- baymax_basic_operations/baymax_file.py
+++ baymax_basic_operations/baymax_file.py
@@ -4,30 +4,38 @@ from Vocal_and_commands.Vocals import Vocals
 
 
 class FileManager:
+    def __init__(self):
+        self.vocal = Vocals()
+
     def create_file(self, filename: str):
         with open(f'{filename}', 'w') as file:
             file.write("This file was created by Baymax File Management system")
-        vocal = Vocals()
-        vocal.speak(f'{filename}, has been created')
+        self.vocal.speak(f'{filename}, has been created')
 
     def read_file(self, filename: str):
         with open(f'{filename}', 'r') as file:
             content = file.read()
-        vocal = Vocals()
-        vocal.speak(f'{filename}, has been read')
+        self.vocal.speak(f'{filename}, has been read')
         return content
 
     def update_file(self, filename: str, content: str):
         with open(f'{filename}', '+a') as file:
             file.write(f" {content}")
-        vocal = Vocals()
-        vocal.speak(f'{filename}, has been updated')
+        self.vocal.speak(f'{filename}, has been updated')
 
     def delete_file(self, filename: str):
         os.remove(filename)
-        vocal = Vocals()
-        vocal.speak(f'{filename}, has been deleted')
+        self.vocal.speak(f'{filename}, has been deleted')
 
 
-fm = FileManager()
-fm.delete_file('test.txt')
+class DirectoryManger:
+    def __init__(self):
+        self.vocal = Vocals()
+
+    def create_dir(self, dir_name):
+        os.mkdir(f'{dir_name}')
+        self.vocal.speak(f"{dir_name} directory has been created")
+
+
+dm = DirectoryManger()
+dm.create_dir('test')
